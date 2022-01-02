6e233fa8dcd870c4d0245b5d03ce6e1636e61737
TastSong <tastsong@163.com>
2022-01-02 21:27:38 +0800

    原型模式

---
+++ Assets/DesignPatternsRevisited/ObserverPattern/ObserverPattern.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ObserverPattern : MonoBehaviour
+{
+    void Start()
+    {
+        ConcreteSubject s = new ConcreteSubject();
+
+        s.Attach(new ConcreteObserver(s, "X"));
+        s.Attach(new ConcreteObserver(s, "Y"));
+        s.Attach(new ConcreteObserver(s, "Z"));
+
+        // Change subject and notify observers
+        s.SubjectState = "ABC";
+        s.Notify();
+        // Change subject and notify observers again
+        s.SubjectState = "666";
+        s.Notify();
+    }
+}
+
+/// <summary>
+/// The 'Subject' abstract class
+/// </summary>
+abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in _observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        // Gets or sets subject state
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        // Constructor
+        public ConcreteObserver(
+          ConcreteSubject subject, string name)
+        {
+            this._subject = subject;
+            this._name = name;
+        }
+
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+            Debug.Log("Observer "+ _name+"'s new state is "+_observerState);
+    }
+
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+
---
+++ Assets/DesignPatternsRevisited/PrototypePattern/PrototypePattern.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrototypePattern : MonoBehaviour
+{
+
+    void Start( )
+    {
+        // Create two instances and clone each
+
+        ConcretePrototype1 p1 = new ConcretePrototype1("I");
+        ConcretePrototype1 c1 = (ConcretePrototype1)p1.Clone();
+        Debug.Log("Cloned: "+c1.Id);
+
+        ConcretePrototype2 p2 = new ConcretePrototype2("II");
+        ConcretePrototype2 c2 = (ConcretePrototype2)p2.Clone();
+        Debug.Log("Cloned: "+c2.Id);
+
+    }
+}
+
+/// <summary>
+/// The 'Prototype' abstract class
+/// </summary>
+abstract class Prototype
+{
+    private string _id;
+
+    // Constructor
+    public Prototype(string id)
+    {
+        this._id = id;
+    }
+
+    // Gets id
+    public string Id
+    {
+        get { return _id; }
+    }
+
+    public abstract Prototype Clone();
+}
+
+/// <summary>
+/// A 'ConcretePrototype' class 
+/// </summary>
+class ConcretePrototype1 : Prototype
+{
+    // Constructor
+    public ConcretePrototype1(string id)
+      : base(id)
+    {
+    }
+
+    // C# 支持的克隆
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
+
+/// <summary>
+/// A 'ConcretePrototype' class 
+/// </summary>
+class ConcretePrototype2 : Prototype
+{
+    // Constructor
+    public ConcretePrototype2(string id)
+      : base(id)
+    {
+    }
+
+    // Returns a shallow copy
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
