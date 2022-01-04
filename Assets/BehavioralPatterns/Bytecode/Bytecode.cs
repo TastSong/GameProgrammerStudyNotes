533ee0e453d23cb91ae8ef8507f84768ba753709
TastSong <tastsong@163.com>
2022-01-04 13:52:09 +0800

    字节码模式

--- Assets/BehavioralPatterns/Bytecode/Bytecode.cs
+++ Assets/BehavioralPatterns/Bytecode/Bytecode.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bytecode : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+enum Instruction {
+    INST_SET_HEALTH = 0x00,
+    INST_SET_WISDOM = 0x01,
+    INST_SET_AGILITY = 0x02,
+    INST_PLAY_SOUND = 0x03,
+    INST_SPAWN_PARTICLES = 0x04
+};
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class Bytecode : MonoBehaviour{
+    void Interpret(Instruction[] instructions) {
+        for (int i = 0; i < instructions.Length; i++) {
+            Instruction instruction = instructions[i];
+            switch (instruction) {
+                case Instruction.INST_SET_HEALTH:
+                    //setHealth(0, 100);
+                    break;
+
+                case Instruction.INST_SET_WISDOM:
+                    //setWisdom(0, 100);
+                    break;
+
+                case Instruction.INST_SET_AGILITY:
+                    //setAgility(0, 100);
+                    break;
+
+                case Instruction.INST_PLAY_SOUND:
+                    //playSound(SOUND_BANG);
+                    break;
+
+                case Instruction.INST_SPAWN_PARTICLES:
+                    //spawnParticles(PARTICLE_FLAME);
+                    break;
+            }
+        }
     }
 }
