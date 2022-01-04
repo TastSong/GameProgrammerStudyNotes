1c960b99b39a2d84ce8f2e23fa3fa5310f2b6358
TastSong <tastsong@163.com>
2022-01-04 17:27:48 +0800

    事件队列

---
+++ Assets/DecouplingPatterns/EventQueue/EventQueueManager.cs
@@ -0,0 +1,156 @@
+﻿//-------------------------------------------------------------------------------------
+//	EventQueueManager.cs
+// Reference :https://github.com/GandhiGames/message_queue
+// http://gandhigames.co.uk/message-queue/
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace EventQueuePattern
+{
+
+    /// <summary>
+    /// 事件Manager
+    /// </summary>
+    public class EventQueueManager
+    {
+        private static EventQueueManager _instance;
+        public static EventQueueManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new EventQueueManager();
+                }
+                return _instance;
+            }
+        }
+
+        //泛型代理
+        public delegate void EventDelegateX<T>(T e) where T : GameEvent;
+        //普通代理
+        private delegate void EventDelegateX(GameEvent e);
+
+        private Dictionary<System.Type, EventDelegateX> DelegatesMap = new Dictionary<System.Type, EventDelegateX>();
+        private Dictionary<System.Delegate, EventDelegateX> DelegateLookupMap = new Dictionary<System.Delegate, EventDelegateX>();
+
+        /// <summary>
+        /// 添加Listener
+        /// </summary>
+        public void AddListener<T>(EventDelegateX<T> del) where T : GameEvent
+        {
+
+            EventDelegateX internalDelegate = (e) => { del((T)e); };
+
+            //已存在,返回
+            if (DelegateLookupMap.ContainsKey(del) && DelegateLookupMap[del] == internalDelegate)
+            {
+                return;
+            }
+
+            //加入delegateLookup中
+            DelegateLookupMap[del] = internalDelegate;
+
+            //加入delegates中
+            EventDelegateX tempDel;
+            if (DelegatesMap.TryGetValue(typeof(T), out tempDel))
+            {
+                DelegatesMap[typeof(T)] = tempDel += internalDelegate;
+            }
+            else {
+                DelegatesMap[typeof(T)] = internalDelegate;
+            }
+        }
+
+        /// <summary>
+        /// 删除Listener
+        /// </summary>
+        public void RemoveListener<T>(EventDelegateX<T> del) where T : GameEvent
+        {
+            EventDelegateX internalDelegate;
+            if (DelegateLookupMap.TryGetValue(del, out internalDelegate))
+            {
+                EventDelegateX tempDel;
+                if (DelegatesMap.TryGetValue(typeof(T), out tempDel))
+                {
+                    tempDel -= internalDelegate;
+                    if (tempDel == null)
+                    {
+                        DelegatesMap.Remove(typeof(T));
+                    }
+                    else {
+                        DelegatesMap[typeof(T)] = tempDel;
+                    }
+                }
+
+                DelegateLookupMap.Remove(del);
+            }
+        }
+        /// <summary>
+        /// 在队列中加入事件
+        /// </summary>
+        public void AddEventToQueue(GameEvent e)
+        {
+            EventDelegateX del;
+            if (DelegatesMap.TryGetValue(e.GetType(), out del))
+            {
+                del.Invoke(e);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 事件优先级枚举
+    /// </summary>
+    public enum MessagePriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    /// <summary>
+    /// 事件接口
+    /// </summary>
+    public interface IMessageEvent
+    {
+        DateTime timeRaised { get; }
+        float displayTime { get; }
+        MessagePriority priority { get; }
+        object message { get; }
+    }
+
+    /// <summary>
+    /// 事件实体类
+    /// </summary>
+    public class MessageEvent : GameEvent, IMessageEvent
+    {
+        public DateTime timeRaised { private set; get; }
+        public MessagePriority priority { private set; get; }
+        public float displayTime { private set; get; }
+        public object message { private set; get; }
+
+        public MessageEvent(object message, float displayTime, MessagePriority priority)
+        {
+            this.message = message;
+            this.displayTime = displayTime;
+            this.priority = priority;
+            timeRaised = DateTime.Now;
+        }
+    }
+
+
+    /// <summary>
+    /// 事件抽象类
+    /// </summary>
+    public abstract class GameEvent
+    {
+
+    }
+
+}
---
+++ Assets/DecouplingPatterns/EventQueue/EventQueuePattern.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace EventQueuePattern
+{
+    public class EventQueuePattern : MonoBehaviour
+    {
+        private Dictionary<MessagePriority, string> priorityLookup = new Dictionary<MessagePriority, string>();
+
+        void Start()
+        {
+            //添加优先级
+            priorityLookup.Add(MessagePriority.Low, "Low");
+            priorityLookup.Add(MessagePriority.Medium, "Medium");
+            priorityLookup.Add(MessagePriority.High, "High");
+        }
+
+        void Update()
+        {
+            //按钮响应
+            if (Input.GetMouseButtonDown(0))
+            {
+                //随机选取优先级
+                MessagePriority priority = MessagePriority.High;
+                float random = UnityEngine.Random.value;
+                if (random < 0.333f)
+                {
+                    priority = MessagePriority.Low;
+                }
+                else if (random < 0.666f)
+                {
+                    priority = MessagePriority.Medium;
+                }
+
+                //随机选取显示时间
+                float showTime = UnityEngine.Random.Range(1f, 5f);
+
+                //加入事件到队列
+                EventQueueManager.Instance.AddEventToQueue(new MessageEvent(priorityLookup[priority] + " priority message shown at "
+                    + System.DateTime.Now + " for " + showTime + " seconds", Time.time + showTime, priority));
+            }
+        }
+    }
+}
