90bf535afedac55c9bcb8311f7317ebb2686b28a
TastSong <tastsong@163.com>
2022-01-05 10:25:20 +0800

    对象池模式

---
+++ Assets/OptimizationPatterns/ObjectPool/Script/Example/ObjectPool.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ObjectPoolPattern
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        //池名称
+        public string poolName;
+        //对象List
+        // public List<GameObject> ObjectList = new List<GameObject>();
+        public List<PoolObject> ObjectList = new List<PoolObject>();
+
+        public void Start()
+        {
+            //初始化对象池
+            PoolManager.Instance.Init();
+            //开始运行时，先从池中取出一个对象
+            GetObjectFromPool();
+        }
+
+
+        /// <summary>
+        /// 从池中取出对象，并设置随机位置
+        /// </summary>
+        public void GetObjectFromPool()
+        {
+            if (ObjectList == null)
+            {
+                return;
+            }
+
+            //设置随机位置
+            Vector3 pos = new Vector3();
+            pos.x = Random.Range(-5, 6);
+            pos.y = 0f;
+            pos.z = Random.Range(-5, 6);
+
+            PoolObject po = PoolManager.Instance.GetObjectFromPool(poolName, pos, Quaternion.identity);
+            if (po)
+            {
+                ObjectList.Add(po);
+            }
+        }
+
+        /// <summary>
+        /// 将索引为0的对象返回池中
+        /// </summary>
+        public void ReturnOneObjectToPool()
+        {
+            if (ObjectList == null || ObjectList.Count <= 0)
+            {
+                return;
+            }
+
+            PoolManager.Instance.ReturnObjectToPool(ObjectList[0]);
+            ObjectList.Remove(ObjectList[0]);
+        }
+
+        /// <summary>
+        /// 将所有对象返回池中
+        /// </summary>
+        public void ReturnAllObjectToPool()
+        {
+            if (ObjectList == null)
+            {
+                return;
+            }
+
+            foreach (PoolObject po in ObjectList)
+            {
+                PoolManager.Instance.ReturnObjectToPool(po);
+            }
+            ObjectList.Clear();
+        }
+    }
+}
